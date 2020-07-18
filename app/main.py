e253c2e7a9810ef74d9a6773a053472ec85bdbc6
namithakonda09 <namithakonda0@gmail.com9>
2020-07-19 00:07:26 +0530

    v5

--- app/main.py
+++ app/main.py
@@ -23,6 +23,13 @@ def page_not_found(e):
     return json.dumps(error), 404, {'Content-Type': 'application/json'}
 
 
+@api.errorhandler(503)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    error = "{'Response':'Application Error'}"
+    return json.dumps(error), 503, {'Content-Type': 'application/json'}
+
+
 @api.errorhandler(500)
 def internal_server_error(e):
     # note that we set the 500 status explicitly
